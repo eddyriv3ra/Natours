4ce6324a5297247fea485fe8e19c67acbeafbba3
unknown <eddy.rivera.echegaray@gmail.com>
2018-12-04 17:44:42 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+          Some text...
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
