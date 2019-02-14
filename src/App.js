7947baa046a38ea642d24482d9d2722c645204fb
unknown <eddy.rivera.echegaray@gmail.com>
2019-02-14 11:54:21 -0300

    BEM

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import imageLogo from './img/logo-white.png'
 
 class App extends Component {
   render() {
     return (
       <div>
         <header className="header">
-          Some text...
+          <div className="header__logo-box">
+            <img src={imageLogo} alt="Logo" className='header__logo'/>
+          </div>
+          <div className='header__text-box'>
+            <h1 className="heading-primary">
+              <span className="heading-primary--main">Outdoors</span>
+              <span className="heading-primary--sub"> is where life happens</span>
+            </h1>
+            <a href="#" className="btn btn--white btn--animated">Discover our tours</a>
+          </div>
         </header>
       </div>
     );
